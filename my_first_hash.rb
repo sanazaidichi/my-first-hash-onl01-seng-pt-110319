aa66b211ac68ec6610ab275658e022ac65ba0e60
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-22 05:40:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest["jar of molasses"] = 10
+  shipping_manifest["muskets"] = 2
  
 puts shipping_manifest
 
