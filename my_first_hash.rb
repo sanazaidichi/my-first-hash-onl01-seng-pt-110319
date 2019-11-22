3a837a85ac07cf8599afc1ae48a256eb1a6aa09b
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-22 05:39:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,9 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["jar of molasses"] = 10
+ 
+puts shipping_manifest
 
 
 
